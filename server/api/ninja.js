041cde0fe1cb0cf2542f41cc1cdefdf960a65e5f
shakiroye <oyeossiadewoye@gmail.com>
2023-02-07 12:15:17 +0000

    initial commit

--- server/api/ninja.js
+++ server/api/ninja.js
@@ -6,8 +6,10 @@ export default defineEventHandler(async (event) => {
   //   const { age } = await readBody(event);
 
   // api call with private key
+  const { currencyKey } = useRuntimeConfig();
+
   const { data } = await $fetch(
-    "https://api.currencyapi.com/v3/latest?apikey=lNG9NQlX934vMNAAsJ0Ddp6LneBiWXmTKJDRiXpv"
+    `https://api.currencyapi.com/v3/latest?apikey=${currencyKey}`
   );
 
   return data;
